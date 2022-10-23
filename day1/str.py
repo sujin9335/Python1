dbce1921f92a2135347b9b338c9bc48b97dd1c50
DESKTOP-F6R2R2G\user <jjh123579@snu.ac.kr>
2022-10-23 10:36:06 +0900

    10.23

---
+++ day1/str.py
@@ -0,0 +1,65 @@
+# print("1"+"1")
+# print("1+1")
+# msg = "페이지 공사중입니다."
+# msg_01 = "시작페이지 입니다."
+# msg_02 = "파이썬 기초 수업 입니다."
+
+# print(msg_01)
+
+# print('파이"썬"')
+# print("파이'썬'")
+# print('파\t이썬 \n 쉽네') #\n 엔터 \t 텝
+# print('파\"이썬\" 쉽네') 
+
+# print('파이썬')
+# print('''파이썬
+#         쉽네''')
+# print("""파이썬""")
+
+
+# word_01 = "python"
+# word_02 = "is fun"
+
+# print(word_01 + word_02)
+# print(word_01, word_02)
+# print(word_01 * 2)
+
+# print(word_01, type(word_01))
+
+# print("="*20)
+# print(word_01 * 2)
+
+# msg = "python is fun"
+# print(msg[6],msg[7],msg[8],msg[-2])
+# print(msg[13])
+
+# print(msg[0:5]) # 0이상 5미만
+# msg = "1234567890"
+# print(msg[::2])
+
+#예제슬라이싱
+# a = "20221225christmas"
+# date = a[:8]
+# day = a[8:]
+# print(date)
+# print(day)
+
+#포메팅
+# print("%s은 %s입니다."%(date,day) ) #문자열 포매팅
+# print("%d은 %s입니다."%(int(date),day) ) #%d 는 정수만
+
+# print("{}은 {}입니다.".format(date,day))
+# print(f"{date}은 {day}입니다.")
+
+#예제
+# date = 2022
+# animal = "호랑이"
+# print(f"올해는 {date}년 {animal}의 해 입니다")
+# print("올해는 %d년 %s의 해 입니다"%(date,animal))
+# print("올해는 %s년 %s의 해 입니다"%(date,animal))
+
+word = "python"
+print(word.count('p'))
+print(word.find("p"))
+print(word.index("p"))
+
---
+++ day2/1.py
@@ -0,0 +1,38 @@
+# word = "python"
+# print(word.find('t'))
+
+# id =  "  Mega"
+# result = id.upper() #대문자 변환
+# result2 = id.lower() #소문자 변환
+# result3 = id.strip() #공백 지우기 (앞에 공백만)
+# print(id)
+# print(result)
+# print(result2)
+# print(result3)
+
+# msg = "I love python"
+# print(msg.replace("love","hate"))
+# print(msg)
+
+# num = "1 2 3 4 5 6 7 8 9"
+# num2 = "1:2:3:4:5:6:7:8:9"
+# print(num.split())
+# print(num2.split(":"))
+
+# msg = input("입력해주세요 : ") 
+# print(msg.split())
+# print(msg)
+
+# num = int(input("숫자 입력해주세요 : ") )
+# print(num, type(num))
+
+word1 = input("첫번째 단어 : ")
+word2 = input("두번째 단어 : ")
+word3 = input("세번째 단어 : ")
+
+a = word1.upper()
+b = word2.upper()
+c = word3.upper()
+
+print('='*20)
+print(a[0]+b[0]+c[0])
