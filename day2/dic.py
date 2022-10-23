71e91501bcc3525744d45c622f98f9cb716bafd9
sujin9335 <sujin93359335@gmail.com>
2022-10-23 13:18:49 +0900

    10.23

---
+++ day2/dic.py
@@ -0,0 +1,35 @@
+## {'name':'메가스터디'} 이거는 key:values 다
+# dic_profile = {'name':'메가스터디', 'adress':'강남', 'floor':4}
+# print(dic_profile, type(dic_profile))
+
+# # print(dic_profile['name'])
+# dic_profile["phone"] = "4624-5555"
+# print(dic_profile, type(dic_profile))
+
+# del dic_profile['floor']
+# print(dic_profile)
+
+# print(dic_profile.pop('adress')) #지운거 표시
+# print(dic_profile) #지워진거 확인
+
+
+# dic_profile1 = {'name':'메가스터디', 'adress':'강남', 'floor':4}
+# dic_profile2 = {'name':'삼성', 'adress':'기흥', 'floor':3, 'phon' : '010'}
+
+# dic_profile1.update(dic_profile2)
+# print(dic_profile1)
+# print(dic_profile1["name"])
+
+
+dic_profile1 = {'name':'메가스터디', 'adress':'강남', 'floor':4}
+print(dic_profile1.keys()), type(dic_profile1)
+
+
+dic_profile1 = {'name':'메가스터디', 'adress':'강남', 'floor':4}
+result = dic_profile1.keys()
+dict_to_list = list(result)
+print(dict_to_list)
+
+result_value = dic_profile1.values()
+dict_to_list2 = list(result_value)
+print(dict_to_list2)
---
+++ day2/tuple.py
@@ -0,0 +1,17 @@
+## 튜플은 변경x
+# tuple_sample = ()
+# print(tuple_sample, type(tuple_sample))
+
+# result = divmod(2,3)
+# print(result)
+
+# print(tuple_sample[0:2])
+
+# tuple_sample[1] = 10 #수정
+
+
+tuple_01 = (1,2)
+tuple_02 = (3,4)
+print(tuple_01+tuple_02)
+sum_tuple = tuple_01+tuple_02
+print(sum_tuple)
